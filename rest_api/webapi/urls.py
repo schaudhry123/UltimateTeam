2cd9fe072069c2e85055dd4de299b1fbacdf5dee
Samir Chaudhry <schaudhry123@gmail.com>
2016-04-06 01:40:58 -0500

    Refactored code to use ViewSets

--- rest_api/webapi/urls.py
+++ rest_api/webapi/urls.py
@@ -1,18 +1,16 @@
 from django.conf.urls import url, include
-from rest_framework.urlpatterns import format_suffix_patterns
 from webapi import views
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = format_suffix_patterns([
-	url(r'^$', views.api_root),
-	url(r'^players/$', views.PlayerList.as_view(), name='player-list'),
-	url(r'^players/(?P<pk>[0-9]+)/$', views.PlayerDetail.as_view(), name='player-detail'),
-	url(r'^teams/$', views.TeamList.as_view(), name='team-list'),
-	url(r'^teams/(?P<pk>[0-9]+)/$', views.TeamDetail.as_view(), name='team-detail'),
-	url(r'^users/$', views.UserList.as_view(), name='user-list'),
-	url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
-])
+# Create a router and register our viewsets with it
+router = DefaultRouter()
+router.register(r'players', views.PlayerViewSet)
+router.register(r'teams', views.TeamViewSet)
+router.register(r'users', views.UserViewSet)
 
-# Login and logout views for the browsable API
-urlpatterns += [
-	url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+# The API URLs are now determined automatically by the router.
+# Additionally, we include the login URLs for the browsable API.
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- rest_api/webapi/views.py
+++ rest_api/webapi/views.py
@@ -1,50 +1,35 @@
 from webapi.models import Player, Team
 from webapi.serializers import PlayerSerializer, TeamSerializer, UserSerializer
 from webapi.permissions import IsOwnerOrReadOnly
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from django.contrib.auth.models import User
 
-@api_view(['GET'])
-def api_root(request, format=None):
-	return Response({
-			'users': reverse('user-list', request=request, format=format),
-			'players': reverse('player-list', request=request, format=format),
-			'teams': reverse('team-list', request=request, format=format),
-		})
-
-class UserList(generics.ListAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+	"""
+	This viewset automatically provides 'list' and 'detail' actions
+	"""
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
 
-class UserDetail(generics.RetrieveAPIView):
-	queryset = User.objects.all()
-	serializer_class = UserSerializer
-
-class PlayerList(generics.ListCreateAPIView):
+class PlayerViewSet(viewsets.ModelViewSet):
+	"""
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+    """
 	queryset = Player.objects.all()
 	serializer_class = PlayerSerializer
-	permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+	permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
 
 	def perform_create(self, serializer):
 		serializer.save(owner=self.request.user)
 
-class PlayerDetail(generics.RetrieveUpdateDestroyAPIView):
-	queryset = Player.objects.all()
-	serializer_class = PlayerSerializer
-	permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
-
-class TeamList(generics.ListCreateAPIView):
+class TeamViewSet(viewsets.ModelViewSet):
 	queryset = Team.objects.all()
 	serializer_class = TeamSerializer
-	permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+	permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
 
 	def perform_create(self, serializer):
 		serializer.save(owner=self.request.user)
-
-class TeamDetail(generics.RetrieveUpdateDestroyAPIView):
-	queryset = Team.objects.all()
-	serializer_class = TeamSerializer
-	permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
